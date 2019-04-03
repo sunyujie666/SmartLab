ea58a7f26a0e3408fe931af70d0ea39aeb9d0fb1
syj <1803727297@qq.com>
2019-04-03 09:49:30 +0800

    getMacInfo

---
+++ until/getSystemInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace SmartLab.until
+{
+    class getSystemInfo
+    {
+
+        public static void main(String []args)
+        {
+            Console.WriteLine(GetMacAddress());
+        }
+        public static string GetMacAddress()
+        {
+            try
+            {
+                string strMac = string.Empty;
+                ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+                    if ((bool)mo["IPEnabled"] == true)
+                    {
+                        strMac = mo["MacAddress"].ToString();
+                    }
+                }
+                moc = null;
+                mc = null;
+                return strMac;
+            }
+            catch
+            {
+                return "unknown";
+            }
+        } 
+    }
+}
